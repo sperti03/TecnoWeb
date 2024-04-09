be2b5467000458d2ca5ad6813c53afed4dfce475
Fabzero22 <132613307+Fabzero22@users.noreply.github.com>
2024-04-09 19:58:33 +0200

    Update music.js

--- music-player/music.js
+++ music-player/music.js
@@ -4,18 +4,22 @@ const cardData = [
         artist: "Giuseppe Castiglia",
         imageUrl: "https://i.scdn.co/image/ab67616d0000b273b7634254914c42ba68c0f5f0",
         audioUrl: "ct.mp3",
+        backimg: "https://i.scdn.co/image/ab67616d0000b273b7634254914c42ba68c0f5f0",
     },
     {
         title: "Semu Giarrisi",
         artist: "Fratelli Tarantella",
         imageUrl: "https://i.scdn.co/image/ab67616d0000b2737ba24b8b670a38e814ae3838",
         audioUrl: "giarre.mp3",
+        backimg: "https://i.scdn.co/image/ab67616d0000b2737ba24b8b670a38e814ae3838"
+
     },
    {
         title: "A Megghiu Frutta",
         artist: "Savvo Zauddu",
         imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/44/8a/17/448a17c1-6b86-ec95-c124-f0268be05eb8/cover.jpg/1200x1200bf-60.jpg",
         audioUrl: "frutta.mp3",
+        backimg: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/44/8a/17/448a17c1-6b86-ec95-c124-f0268be05eb8/cover.jpg/1200x1200bf-60.jpg",
     },
 ];
 
@@ -29,20 +33,33 @@ const audioSource = document.getElementById("audioSource");
 const playIcon = document.getElementById("playIcon");
 const pauseIcon = document.getElementById("pauseIcon");
 const volumeControl = document.getElementById("volumeControl");
+const backimage = document.getElementById("backimg");
+const seekBar = document.getElementById("seekBar");
+
+seekBar.addEventListener("input", function() {
+    const newPosition = this.value;
+    audioPlayer.currentTime = newPosition;
+});
+
+audioPlayer.addEventListener("timeupdate", function() {
+    seekBar.value = this.currentTime;
+});
 
 function updateCard() {
-    const { title, artist, imageUrl, audioUrl } = cardData[currentIndex];
+    const { title, artist, imageUrl, audioUrl, backimg } = cardData[currentIndex];
     cardImage.src = imageUrl;
     cardTitle.textContent = title;
     cardText.textContent = artist;
     audioSource.src = audioUrl;
     audioPlayer.load();
     pauseAudio();
-    setBackgroundImage(imageUrl);
+    setBackgroundImage(backimg);
 }
 
 function setBackgroundImage(url) {
-    body.style.backgroundImage = `url(${url})`;
+    const backgroundImage = document.getElementById("backgroundImage");
+    backgroundImage.style.backgroundImage = `url(${url})`;
+    backgroundImage.style.filter = "blur(5px)"; // Cambia il valore per ottenere l'effetto desiderato
 }
 
 function toggleAudio() {
